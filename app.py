7914f526dde02b0adc0441cb1a16f46d63957f7c
atharva2001 <shirkeatharva.study@gmail.com>
2025-11-11 06:48:30 +0000

    structure change

--- app.py
+++ app.py
@@ -1,85 +1,35 @@
-# from fastapi import FastAPI
-# from MCP_Application.app import mcp_server
-# import uvicorn
-
-# app = FastAPI(
-#     title="Fast MCP Leader",
-#     description="A FastAPI application to fetch Leads for recruiters.",
-#     version="0.0.1"
-# )
-
-# mcp_app = mcp_server.http_app()
-
-# app.mount("/mcp")
-
-# @app.get("/")
-# async def read_root():
-#     return {"message": "Welcome to Fast MCP Leader!"}
-
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         "app:app",
-#         host="0.0.0.0",
-#         port=8000,
-#         reload=True
-#     )
-
-from langchain.agents import create_agent
+from fastmcp import FastMCP
+from langchain_community.tools import DuckDuckGoSearchRun, DuckDuckGoSearchResults  
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from langchain_google_genai import ChatGoogleGenerativeAI
-import os
+from langchain.agents import create_agent
 from dotenv import load_dotenv
-import asyncio
-import httpx
+import os
 
 load_dotenv()
 
-cust_client = httpx.Client(verify=False)
+mcp_server = FastMCP(
+    "MCP Application",
+)
 
-llm = ChatGoogleGenerativeAI(
-        model='gemini-2.5-flash',
-        temperature=0.0,
-        api_key=os.environ["GEMINI_API_KEY"],   
-    )
+client = DuckDuckGoSearchResults(output_format="list")
 
-async def main():
-    
-    try:
-        mcp_client = MultiServerMCPClient(
-            {
-                "playwright": {
-                    "url": "http://localhost:8931/mcp",
-                    "transport": "streamable_http",
-                },
+@mcp_server.tool(name="linkedin_search", description="Search LinkedIn for professionals based on a query.")
+def linkedin_search(query: str = "") -> list:
+    '''  Search LinkedIn for professionals based on a query.'''
 
-                "Linkedin_MCP": {
-                    "url": "http://localhost:8080/mcp",
-                    "transport": "streamable_http",
-                }
-            },
-        )
+    if query == "": return "No query provided."
 
-        message = "Find me 5 Python develier."
+    res = client.invoke(query)
 
-        prompt = "You are an expert lead generator. Use the MCP tools to get leads based on user queries."
+    result = []
 
-        tools = await mcp_client.get_tools()
+    for r in res:
+        result.append(r.get("link"))
 
-        llm.bind_tools(tools)
+    return result
 
-        # print("Available tools:")
-        # for tool in tools:
-        #     print(f"  - {tool.name}: {tool.description}")
 
-        res = llm.invoke("" + prompt + "\n" + message)
 
-        print("Final Response: ", res)
-
-    except Exception as e:
-        print("Error: ", e)
-        import traceback
-        traceback.print_exc()
 
 if __name__ == "__main__":
-    
-    asyncio.run(main())
+    mcp_server.run("streamable-http", host="0.0.0.0", port=8080)
